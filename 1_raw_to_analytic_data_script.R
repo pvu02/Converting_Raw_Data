e46c00a63c2b13d45b937aedfeb958404143c46d
Janie Vu <Janie Vu>
2016-09-20 20:48:54 -0400

    This is my second commit after correcting reverse key items.

---
+++ 1_raw_to_analytic_data_script.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+# Load Raw Data File with Corrections for Values Not Indicated
+raw_data <- read_csv(file="rawData.csv",na=c("","NA","-999","-888"))
+
+# Handling Categorical Variables to Factors
+categorical_variables <- select(raw_data, group, gender)
+
+## Factor for Group
+categorical_variables$group <- as.factor(categorical_variables$group)
+
+## Factor for Gender
+categorical_variables$gender <- as.factor(categorical_variables$gender)
+levels(categorical_variables$gender) <- list("Male"=1,"Female"=2)
+
+# Create sets of s cale items to deal with out of range values)
+affective_commitment_items <- select (raw_data, AC1, AC2, AC3, AC4, AC5)
+agreeableness_items <- select (raw_data, A1, A2, A3, A4, A5)
+extroversion_items <- select (raw_data, E1, E2, E3, E4, E5)
+
+# Inspect variable items
+psych::describe(extroversion_items)
+psych::describe(agreeableness_items)
+
+# Correcting for outliers in Agreeableness Scale
+is_bad_value <- agreeableness_items<1 | agreeableness_items>5
+agreeableness_items[is_bad_value] <- NA
+
+# Inspect variable items
+psych::describe(affective_commitment_items)
+
+# Correcting for outliers in Affective Commitment Scale
+is_bad_value <- affective_commitment_items<1 | affective_commitment_items>7
+affective_commitment_items[is_bad_value] <- NA
+
+# Correcting for Reverse-Key Items
+agreeableness_items <- mutate(agreeableness_items,A5=6-A5)
+affective_commitment_items <- mutate(affective_commitment_items,AC4=8-A4)
+affective_commitment_items <- mutate(affective_commitment_items,AC5=8-A5)
